1089373b06a24df7cf01f18362fc2df5237d7234
emircanalcan <alcan.emircan@gmail.com>
2024-12-03 01:04:03 +0300

    3

--- Assets/Scripts/alpagu.cs
+++ Assets/Scripts/alpagu.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 public class alpagu : MonoBehaviour
 {
+    #region var
     public Rigidbody2D alpagurigidbody2D;
+    public GameObject alpagu_arrow;
+    public GameObject alpagu_swordslash;
+    private bool bool_weaponbow = false;
+    private float float_weaponrate = 0.5f;
+    private float float_timer = 0;
+    #endregion
+
+    #region func
+    public void func_weapon(GameObject prefab, float offset, bool weaponbow)
+    {
+        if (Input.GetKey(KeyCode.Mouse0)|| Input.GetKey(KeyCode.M))
+        {
+            Instantiate(prefab, transform.position + new Vector3(offset, 0, 0), transform.rotation);
+            float_timer = 0;
+        }
+        if (Input.GetKey(KeyCode.Mouse1)|| Input.GetKey(KeyCode.N))
+        {
+            bool_weaponbow = weaponbow;
+            float_timer = 0;
+        }
+    }
     void Start()
     {
 
     }
     void Update()
     {
+        #region movement
         Vector2 movement = Vector2.zero;
         if (Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.W))
         {
@@ -28,5 +51,25 @@ public class alpagu : MonoBehaviour
             movement.x = 1;
         }
         alpagurigidbody2D.velocity = movement.normalized * 8f;
+        #endregion
+
+        #region weapon
+        if (float_weaponrate > float_timer)
+        {
+            float_timer += Time.deltaTime;
+        }
+        else
+        {
+            if (bool_weaponbow)
+            {
+                func_weapon(alpagu_arrow, 1, false);
+            }
+            else
+            {
+                func_weapon(alpagu_swordslash, 2, true);
+            }
+        }
+        #endregion
     }
+    #endregion
 }
