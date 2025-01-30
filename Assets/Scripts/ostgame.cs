71b0833c0abf82d3ce27d3c8d5a93a35c9ff2865
emircanalcan <alcan.emircan@gmail.com>
2025-01-30 23:12:22 +0300

    18

---
+++ Assets/Scripts/ostgame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ostgame : MonoBehaviour
+{
+    public AudioSource ostgame_;
+    void Update()
+    {
+        ostgame_.Play();
+    }
+}
---
+++ Assets/Scripts/ostmain.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ostmain : MonoBehaviour
+{
+    public AudioSource ostmain_;
+    void Start()
+    {
+        ostmain_.Play();
+    }
+}
